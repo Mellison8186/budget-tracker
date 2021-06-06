a417ff6a7bcbffc9785b96d3358657361f047c47
maribels_are_ringing <maribel.duron86@gmail.com>
2021-06-06 13:52:20 -0700

    fixed fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "data-cache-" + VERSION;
 const FILES_TO_CACHE = [
     "./index.html",
     "./css/styles.css",
@@ -36,18 +37,47 @@ self.addEventListener('activate', function (e) {
 
 self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
+
+if (e.request.url.includes("/api/")) {
     e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
+        caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(e.request).then(response => {
+                if (response.status === 200) {
+                    cache.put(e.request.url, response.clone())
+                }
+                return response
+            })
+            .catch(err => {
+                return cache.match(e.request)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
     )
-  })
+    return
+}
+    e.respondWith(
+        fetch(e.request).catch(function(){
+            return caches.match(e.request).then(function (response) {
+                if (response) {
+                    return resposne
+                } else {
+                    return caches.match
+                }
+            })
+        })
+    //     if (request) { // if cache is available, respond with cache
+    //       console.log('responding with cache : ' + e.request.url)
+    //       return request
+    //     } else {       // if there are no cache, try fetching request
+    //       console.log('file is not cached, fetching : ' + e.request.url)
+    //       return fetch(e.request)
+    //     }
+  
+    //     // You can omit if/else for console.log & put one line below like this too.
+    //     // return request || fetch(e.request)
+    //   })
+    // )
+)}
+)
