cdb2908f223ca0a2e77c5a22f838b988433a954f
Maribel <maribel.duron86@gmail.com>
2023-12-22 19:37:32 -0600

    updated mongodb driver

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mellison8186:Sno4T2haT1ZlU0hjbzlEcA==@cluster0.l3goa2z.mongodb.net/?retryWrites=true&w=majority";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://mellison8186:Sno4T2haT1ZlU0hjbzlEcA%3D%3D@cluster0.l3goa2z.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 
